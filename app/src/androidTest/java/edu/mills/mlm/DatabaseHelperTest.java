2c346234e299865f515262e56ec6631ad4b548c6
Ellen Spertus <spertus@mills.edu>
2019-04-01 21:24:45 -0700

    Complete implementation for Lecture 11

---
+++ app/src/androidTest/java/edu/mills/mlm/DatabaseHelperTest.java
@@ -0,0 +1,43 @@
+package edu.mills.mlm;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseHelperTest {
+    private static final String EMAIL1 = "susan@mills.edu";
+    private static final String NAME1 = "Susan Mills";
+    private static final Context context = InstrumentationRegistry.getTargetContext();
+    private DatabaseHelper helper;
+    private SQLiteDatabase db;
+
+    @Before
+    public void setUp() {
+        helper = new DatabaseHelper(context);
+        db = helper.getWritableDatabase();
+        assertTrue(true);
+    }
+
+    @After
+    public void tearDown() {
+        helper.deleteAllSubscribers(db);
+        db.close();
+    }
+
+    @Test
+    public void testAddingSubscriber() {
+        assertFalse(
+                EMAIL1 + " should not be subscribed in new database.",
+                helper.isSubscribed(db, EMAIL1));
+        helper.addSubscriber(db, EMAIL1, NAME1);
+        assertTrue(
+                EMAIL1 + " should be subscribed after adding to database.",
+                helper.isSubscribed(db, EMAIL1));
+    }
+}
--- app/src/main/java/edu/mills/mlm/DatabaseHelper.java
+++ app/src/main/java/edu/mills/mlm/DatabaseHelper.java
@@ -6,12 +6,17 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "mlm";
     private static final int DB_VERSION = 1;
     private static final String SUBSCRIBERS_TABLE = "subscribers";
-    static final String EMAIL_COL = "email";
-    static final String NAME_COL = "names";
+    private static final String EMAIL_COL = "email";
+    private static final String NAME_COL = "names";
 
     DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -30,7 +35,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         if (oldVersion < 1) {
             db.execSQL("CREATE TABLE " + SUBSCRIBERS_TABLE +
                     " (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + EMAIL_COL + " TEXT PRIMARY KEY, "
+                    + EMAIL_COL + " TEXT UNIQUE, "
                     + NAME_COL + " TEXT); ");
         }
     }
@@ -41,7 +46,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         values.put(NAME_COL, name);
         db.insert(SUBSCRIBERS_TABLE, null, values);
     }
-
+/*
     boolean isSubscribed(SQLiteDatabase db, String email) {
         Cursor cursor = db.query(SUBSCRIBERS_TABLE,
                 new String[]{}, // columns to retrieve
@@ -54,19 +59,59 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         cursor.close();
         return result;
     }
+    */
 
-    boolean isSubscribed2(SQLiteDatabase db, String email) {
-        try (
-        Cursor cursor = db.query(SUBSCRIBERS_TABLE,
-                new String[]{}, // columns to retrieve
-                EMAIL_COL + " = ?",  // selection
-                new String[]{email},  // selection args
-                null,  // group by
-                null,   // having
-                null)) { // order by {
+    boolean isSubscribed(SQLiteDatabase db, String email) {
+        try (Cursor cursor = db.query(SUBSCRIBERS_TABLE,
+                new String[]{}, EMAIL_COL + " = ?",
+                new String[]{email}, null, null, null)) {
             return cursor.getColumnCount() == 1;
-        } finally {
-            cursor.close();
         }
     }
+
+    void deleteAllSubscribers(SQLiteDatabase db) {
+        db.delete(SUBSCRIBERS_TABLE, null, null);
+    }
+
+    String getFileContents(String inputFile) throws IOException {
+        isSubscribed(null, null);
+        StringBuilder sb = new StringBuilder();
+        try (Scanner scanner = new Scanner(new File(inputFile))) {
+            while (scanner.hasNext()) {
+                sb.append(scanner.nextLine());
+            }
+            return sb.toString();
+        }
+    }
+/*
+    String getFileContents(String inputFile) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        Scanner scanner = new Scanner(new File(inputFile));
+        while (scanner.hasNext()) {
+            sb.append(scanner.nextLine());
+        }
+        scanner.close();
+        return sb.toString();
+    }
+    */
+
+    /*
+    void printFile(String inputFile) throws IOException {
+        try (Scanner scanner = new Scanner(new File(inputFile))) {
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        }
+    }
+    */
+
+    void copyFile(String inputFile, String outputFile) throws IOException {
+        try (Scanner scanner = new Scanner(new File(inputFile));
+             PrintWriter writer = new PrintWriter(new File(outputFile))) {
+            while (scanner.hasNext()) {
+                writer.print(scanner.nextLine());
+            }
+        }
+    }
+
 }
